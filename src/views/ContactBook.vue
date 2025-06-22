c9fe20385b9298700bec79a24e0d7806923cbc70
puqi1803 <tnpquynh1803@gmail.com>
2025-06-22 12:44:53 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -12,19 +12,21 @@
             v-model:activeIndex="activeIndex"
             />
             <p v-else>Không có liên hệ nào.</p>
-            <div class="mt-3 row justify-content-around align-items-center">
+            
+            <div class="col-auto mt-3 row justify-content-around align-items-center">
 
-            <button class="btn btn-sm btn-primary" @click="refreshList()">
+            <button class="col-auto btn btn-sm btn-primary" @click="refreshList()">
                 <i class="fas fa-redo"></i> Làm mới
             </button>
 
-            <button class="btn btn-sm btn-success" @click="goToAddContact">
+            <button class="col-auto btn btn-sm btn-success" @click="goToAddContact">
                 <i class="fas fa-plus"></i> Thêm mới
             </button>
 
-            <button class="btn btn-sm btn-danger" @click="removeAllContacts">
+            <button class="col-auto btn btn-sm btn-danger" @click="removeAllContacts">
                 <i class="fas fa-trash"></i> Xóa tất cả
             </button>
+            
         </div>
     </div>
 
@@ -34,6 +36,16 @@
                 <i class="fas fa-address-card"></i>
             </h4>
             <ContactCard :contact="activeContact" />
+            <router-link
+                :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                }"
+                >
+                    <span class="mt-2 badge badge-warning">
+                    <i class="fas fa-edit"></i> Hiệu chỉnh</span
+                >
+            </router-link>
             </div>
         </div>
     </div>
