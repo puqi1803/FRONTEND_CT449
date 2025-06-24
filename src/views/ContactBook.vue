3b7d2885bd774b495b31f7353b0b68f07f64b0d6
puqi1803 <tnpquynh1803@gmail.com>
2025-06-24 22:39:04 +0700

    Tao trang them moi mot lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -14,38 +14,32 @@
             <p v-else>Không có liên hệ nào.</p>
             
             <div class="col-auto mt-3 row justify-content-around align-items-center">
+                <button class="col-auto btn btn-sm btn-primary" @click="refreshList()">
+                    <i class="fas fa-redo"></i> Làm mới
+                </button>
+                <button class="col-auto btn btn-sm btn-success" @click="goToAddContact">
+                    <i class="fas fa-plus"></i> Thêm mới
+                </button>
+                <button class="col-auto btn btn-sm btn-danger" @click="removeAllContacts">
+                    <i class="fas fa-trash"></i> Xóa tất cả
+                </button>
+            </div>
+        </div>    
 
-            <button class="col-auto btn btn-sm btn-primary" @click="refreshList()">
-                <i class="fas fa-redo"></i> Làm mới
-            </button>
-
-            <button class="col-auto btn btn-sm btn-success" @click="goToAddContact">
-                <i class="fas fa-plus"></i> Thêm mới
-            </button>
-
-            <button class="col-auto btn btn-sm btn-danger" @click="removeAllContacts">
-                <i class="fas fa-trash"></i> Xóa tất cả
-            </button>
-            
-        </div>
-    </div>
-
-    <div class="mt-3 col-md-6">
-        <div v-if="activeContact">
-            <h4>Chi tiết Liên hệ
-                <i class="fas fa-address-card"></i>
-            </h4>
-            <ContactCard :contact="activeContact" />
-            <router-link
-                :to="{
-                    name: 'contact.edit',
-                    params: { id: activeContact._id },
-                }"
-                >
-                    <span class="mt-2 badge badge-warning">
-                    <i class="fas fa-edit"></i> Hiệu chỉnh</span
-                >
-            </router-link>
+        <div class="mt-3 col-md-6">
+            <div v-if="activeContact">
+                <h4>Chi tiết Liên hệ
+                    <i class="fas fa-address-card"></i>
+                </h4>
+                <ContactCard :contact="activeContact" />
+                <router-link
+                    :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact._id },
+                    }">
+                        <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
